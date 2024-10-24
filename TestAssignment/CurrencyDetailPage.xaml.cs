6a16b0d59bc7bf6f44fdc99491082d3ce8ddc0b2
Admin <Admin@DESKTOP-S21DPMQ>
2024-10-24 20:27:16 +0300

    Add project files.

---
+++ TestAssignment/CurrencyDetailPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestAssignment
+{
+    /// <summary>
+    /// Interaction logic for CurrencyDetailPage.xaml
+    /// </summary>
+    public partial class CurrencyDetailPage : Page
+    {
+        private CryptoCurrency _crypto;
+
+        public CurrencyDetailPage(CryptoCurrency crypto)
+        {
+            InitializeComponent();
+            _crypto = crypto;
+            LoadCurrencyDetails();
+        }
+
+        private void LoadCurrencyDetails()
+        {
+            NameText.Text = _crypto.Name;
+            PriceText.Text = _crypto.Price.ToString();
+            ChangeText.Text = _crypto.Change.ToString();
+            VolumeText.Text = "Тут буде обсяг торгів"; // Або з API
+
+            // Тут можна додати інформацію про ринки
+            MarketsListView.ItemsSource = new List<string>
+        {
+            "Binance - $30100",
+            "Coinbase - $30050",
+            "Kraken - $30080"
+        };
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NavigationService.CanGoBack)
+            {
+                NavigationService.GoBack();  // Повертає на попередню сторінку
+            }
+        }
+    }
+}
---
+++ TestAssignment/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestAssignment
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public List<CryptoCurrency> ?TopCryptos { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadTopCryptos(); // Завантаження даних
+        }
+
+        private void LoadTopCryptos()
+        {
+            // Тут ви можете підключити API для отримання криптовалют
+            // Приклад: завантаження тестових даних
+            TopCryptos = new List<CryptoCurrency>
+        {
+            new CryptoCurrency { Rank = 1, Name = "Bitcoin", Price = 30000, Change = 1.2 },
+            new CryptoCurrency { Rank = 2, Name = "Ethereum", Price = 2000, Change = 2.3 },
+            // додати більше валют
+        };
+
+            cryptoDataGrid.ItemsSource = TopCryptos;
+        }
+
+        private void CryptoDataGrid_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (cryptoDataGrid.SelectedItem is CryptoCurrency selectedCrypto)
+            {
+                // Перехід на сторінку детальної інформації
+                NavigationService.Navigate(new CurrencyDetailPage(selectedCrypto));
+            }
+        }
+    }
+}
